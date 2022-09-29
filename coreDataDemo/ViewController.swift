123e8bb69849770526566e1495684720b23a8963
keshew <97288599+keshew@users.noreply.github.com>
2022-09-29 13:50:33 +0500

    Initial Commit

---
+++ coreDataDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coreDataDemo
+//
+//  Created by Артём Коротков on 29.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
